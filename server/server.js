d561124e9c72cc0173f6d7e94188775e12b36485
Marco Pinesso <pinesso.marco@gmail.com>
2017-08-23 13:24:15 +0200

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -23,14 +23,19 @@ app.use(express.static(publicPath)); // middleware
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Marco',
-    text: 'Yooooooo fiqqo',
-    createdAt: 123123
-  });
+  // socket.emit('newMessage', {
+  //   from: 'Marco',
+  //   text: 'Yooooooo fiqqo',
+  //   createdAt: 123123
+  // });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
