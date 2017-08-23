c7d771e336788ae99c2ec94cda7d535d5e4427c6
Marco Pinesso <pinesso.marco@gmail.com>
2017-08-23 12:40:54 +0200

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,22 +1,33 @@
 // librerie di node
 const path = require('path');
+const http = require('http');
 
 // librerie esterne
 const express = require('express');
+const socketIO = require('socket.io');
 
 // costanti
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // variabili
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // configurazione del server
   // indica dove sono situati i file statici (html, img, ...)
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); // middleware
 
-
+///////////////////////////////////////////////////////////////////
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+///////////////////////////////////////////////////////////////////
 // configura la porta del server
-app.listen(port, () =>{
+server.listen(port, () =>{
   console.log(`Started on port ${port}`);
 })
